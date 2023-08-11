22f29507dbf790989fd982f96b86edeb3e62cd57
Binyam.Hailu <binyam.hailu@partner.safaricom.et>
2023-08-11 10:03:49 +0300

    updated the pagination query

--- nest-book-api/src/app.module.ts
+++ nest-book-api/src/app.module.ts
@@ -3,18 +3,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Books } from './books/entities/book.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'changeme',
-      database: 'postgres',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // will Set it to false in production
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [Books],
+        synchronize: true,
+      }),
     }),
     BooksModule,
   ],
